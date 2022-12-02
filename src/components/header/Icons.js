8285ea08aa3a35bb134b66a4a7f8ede3a8296d4f
Nursen <nursenokenov57@gmail.com>
2022-12-02 23:40:21 +0300

    Write code Icons.js

---
+++ src/components/header/Icons.js
@@ -0,0 +1,11 @@
+import IMAGE1 from "../../assets/IMAGE (1).png";
+import IMAGE2 from "../../assets/IMAGE (2).png";
+function Icons() { 
+    return (
+        <div class="icons__header">
+            <img src={IMAGE1} alt="/assets/IMAGE (1).png" />
+            <img src={IMAGE2} alt="/assets/IMAGE (2).png" />
+          </div>
+    );
+}
+export default Icons;
