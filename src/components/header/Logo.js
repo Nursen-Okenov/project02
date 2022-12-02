a6b23f1c922f77a528f51f8fbfce941548c5f820
Nursen <nursenokenov57@gmail.com>
2022-12-02 23:07:06 +0300

    Write code Logo.js

---
+++ src/components/header/Logo.js
@@ -0,0 +1,10 @@
+import IMAGE from "../../assets/IMAGE.png";
+
+function Logo() {
+    return (
+        <div class="image__header">
+            <img src={IMAGE} alt="assets/IMAGE.png" />
+          </div>
+    );
+}
+export default Logo;
