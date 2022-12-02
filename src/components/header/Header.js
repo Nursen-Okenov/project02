163b7f6b6b645e2203550dee6ba99bd13e2317d5
Nursen <nursenokenov57@gmail.com>
2022-12-02 23:41:59 +0300

    Create file Header.js and write code

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import Logo from "./Logo";
+import Nav from "./Nav";
+import Icons from "./Icons";
+
+function Header() {
+    return (
+      <header class="header">
+        < Logo />
+        < Nav />
+        < Icons />
+      </header>
+    );
+}
+export default Header;
