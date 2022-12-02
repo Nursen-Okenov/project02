62397a645cb20fca27cbdd0a5c1f1f71a7ab5878
Nursen <nursenokenov57@gmail.com>
2022-12-02 23:35:42 +0300

    Write code Nav.js

---
+++ src/components/header/Nav.js
@@ -0,0 +1,17 @@
+function Nav() {
+    return (
+        <nav class="navigation">
+          <div class="menu__header">
+            <ul>
+              <li><a href="">ALL PRODUCTS</a></li>
+              <li>
+                <a href="">SOLUTIONS</a>
+              </li>
+              <li><a href="">ABOUT</a></li>
+              <li><a href="">SUPPORT</a></li>
+            </ul>
+          </div>
+        </nav>
+    );
+}
+export default Nav;
