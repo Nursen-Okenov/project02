7d2a7a70f16646517db657ca18c878371f21424d
Nursen-Okenov <nursenokenov57@gmail.com>
2022-12-05 00:35:14 +0300

    connect and import files

--- src/components/main/fifth__block/Fifth__Block.js
+++ src/components/main/fifth__block/Fifth__Block.js
@@ -1,9 +1,11 @@
+import Fifth__Containers from "./fifth__containers/Fifth__Containers";
 import Text__Fifth from "./text__fifth/Text__Fifth";
 
 function Fifth__Block() {
     return (
         <section class="flex_container_3">
             < Text__Fifth />
+            < Fifth__Containers />
         </section>
     );
 }
