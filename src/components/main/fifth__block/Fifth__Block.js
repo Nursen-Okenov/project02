94cc63cdf3b100d1f1ef7354ac07ad457c5a1b33
Adina <meiadzumi@gmail.com>
2022-12-05 00:18:50 +0300

    Write code fifth__block.js and create new package and file and import

---
+++ src/components/main/fifth__block/Fifth__Block.js
@@ -0,0 +1,10 @@
+import Text__Fifth from "./text__fifth/Text__Fifth";
+
+function Fifth__Block() {
+    return (
+        <section class="flex_container_3">
+            < Text__Fifth />
+        </section>
+    );
+}
+export default Fifth__Block;
