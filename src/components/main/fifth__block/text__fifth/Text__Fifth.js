02604b35ebc152b9e9fbe4eebfb060e93f9f42cb
Adina <meiadzumi@gmail.com>
2022-12-05 00:20:16 +0300

    create new package and file, write code

---
+++ src/components/main/fifth__block/text__fifth/Text__Fifth.js
@@ -0,0 +1,9 @@
+function Text__Fifth() {
+    return (
+        <div class="text_center_4">
+            <h1>Ideas have a place here</h1>
+            <p>Our new Limited Edition Winter Design BESPOKE 4-Door Flex™</p>
+        </div>
+    );
+}
+export default Text__Fifth;
