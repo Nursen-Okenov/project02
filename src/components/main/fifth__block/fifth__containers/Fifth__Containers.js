886184ea49dff0d20d47eea1cc02691ad5bb164b
Nursen-Okenov <nursenokenov57@gmail.com>
2022-12-05 00:32:16 +0300

    connect and import files

--- src/components/main/fifth__block/fifth__containers/Fifth__Containers.js
+++ src/components/main/fifth__block/fifth__containers/Fifth__Containers.js
@@ -1,7 +1,11 @@
+import Image__Fifth from "./Image__Fifth";
+import Paragrah__Fifth from "./Paragrah__Fifth";
+
 function Fifth__Containers() {
     return (
         <div class="two_bocks">
-            
+            < Image__Fifth />
+            < Paragrah__Fifth />
         </div>
     );
 }
