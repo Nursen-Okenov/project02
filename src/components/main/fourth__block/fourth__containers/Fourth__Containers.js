af2a6741ae5a0337dd59b4d0e85cbbfe95155396
Adina <meiadzumi@gmail.com>
2022-12-04 23:58:49 +0300

    import and connect some code

--- src/components/main/fourth__block/fourth__containers/Fourth__Containers.js
+++ src/components/main/fourth__block/fourth__containers/Fourth__Containers.js
@@ -1,7 +1,13 @@
+import First__Box from "./first__box/First__Box";
+import Second__Box from "./second__box/Second__Box";
+import Third__Box from "./third__box/Third__box";
+
 function Fourth__Containers() {
     return (
         <div class="three_bokcs">
-
+            < First__Box />
+            < Second__Box />
+            < Third__Box />
         </div>
     );
 }
