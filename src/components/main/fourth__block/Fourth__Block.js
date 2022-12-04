d3c39e016371a3e61b85af7572e1827b7f0da2dd
Adina <meiadzumi@gmail.com>
2022-12-05 00:00:30 +0300

    import and connect some code

--- src/components/main/fourth__block/Fourth__Block.js
+++ src/components/main/fourth__block/Fourth__Block.js
@@ -1,7 +1,11 @@
+import Fourth__Containers from "./fourth__containers/Fourth__Containers";
+import Text__Fourth from "./text__fourth/Text__Fourth";
+
 function Fourth__Block() {
     return (
         <section class="flex_container_2">
-            
+            < Text__Fourth />
+            < Fourth__Containers />
         </section>
     );
 }
